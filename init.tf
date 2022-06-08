9e601097ae6c8b986cc7faec16dab37a6526c1c6
salimabdulkareem <salim.abdulkareem@kainos.com>
2022-06-08 11:18:53 +0100

    Added init file

---
+++ init.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.2.0"
+    }
+  }
+  experiments = [module_variable_optional_attrs]
+}
