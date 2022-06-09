020364742623e04472d8064604e51fd3dddc5fc4
salimabdulkareem <salim.abdulkareem@kainos.com>
2022-06-09 11:45:24 +0100

    Removed random provider

--- init.tf
+++ init.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.7.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.2.0"
-    }
   }
   experiments = [module_variable_optional_attrs]
 }
